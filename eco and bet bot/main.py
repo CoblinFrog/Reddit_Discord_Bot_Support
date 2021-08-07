38c271f5d76cc46cf7c1a8cbbbacacbc09bf7f13
Tristan <75189508+realTristan@users.noreply.github.com>
2021-08-07 14:06:51 -0400

    Update main.py

--- eco and bet bot/main.py
+++ eco and bet bot/main.py
@@ -16,7 +16,7 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    with open("eco.json", "r+") as f:
+    with open("json/eco.json", "r+") as f:
         data=json.load(f)
         if not str(member.id) in data:
             data.update({str(member.id) : 100})
@@ -28,10 +28,10 @@ async def on_member_join(member):
 @client.command()
 async def startbet(ctx, *args):
     form_args = list(args); count = ''.join((random.choice(string.ascii_uppercase) for i in range(3)))
-    with open('eco.json', 'r+') as fs:
+    with open('json/eco.json', 'r+') as fs:
         eco_data = json.load(fs); eco_data[str(ctx.author.id)] -= 50 # removes 50$ from their wallet when creating a new bet
 
-    with open("bets.json", "r+") as f:
+    with open("json/bets.json", "r+") as f:
         data=json.load(f)
         data.update({str(count) : {1 : {"players": {}}, 2 : {"players": {}}, "owner" : str(ctx.author.id)}})
         f.seek(0); json.dump(data, f, indent=4); f.truncate(); f.close()
@@ -49,7 +49,7 @@ async def startbet(ctx, *args):
 @client.command()
 async def bet(ctx, *args):
     args = list(args)
-    with open("bets.json", "r+") as f:
+    with open("json/bets.json", "r+") as f:
         data=json.load(f)
 
         if not str(ctx.author.id) in data[str(args[0])][str(args[1])]["players"]:
@@ -61,7 +61,7 @@ async def bet(ctx, *args):
             await ctx.send(embed=discord.Embed(description=f'{ctx.author.mention} bet is now at **{data[str(args[0])][str(args[1])]["players"][str(ctx.author.id)]}$**', color=65280))
             f.seek(0); json.dump(data, f, indent=4); f.truncate(); f.close()
         
-        with open("eco.json", "r+") as eco:
+        with open("json/eco.json", "r+") as eco:
             eco_data = json.load(eco); eco_data[str(ctx.author.id)] -= int(args[2])
             eco.seek(0); json.dump(eco_data, eco, indent=4); eco.truncate(); eco.close()
 
@@ -72,40 +72,34 @@ async def bet(ctx, *args):
 @client.command()
 async def end(ctx, *args):
     args = list(args)
-    with open("bets.json", "r+") as f:
+    with open("json/bets.json", "r+") as f:
         data=json.load(f)
         sum = 0; user_count = 0
+
         if data[str(args[0])]["owner"] == str(ctx.author.id):
-            if str(args[1]) == "1":
-                for user in data[str(args[0])]["1"]["players"]:
-                    user_count += 1
-                    sum += data[str(args[0])]["2"]["players"][str(user)] / user_count
-                    with open("eco.json", "r+") as fs:
-                        data2=json.load(fs); data2[str(user)] += sum
+            with open("json/eco.json", "r+") as fs:
+                data2=json.load(fs)
+                if str(args[1]) == "1":
+                    for user in data[str(args[0])]["1"]["players"]:
+                        user_count += 1
+                        sum += data[str(args[0])]["2"]["players"][str(user)] / user_count
+                        data2[str(user)] += sum
                         fs.seek(0); json.dump(data2, fs, indent=4); fs.truncate(); fs.close()
-                
-                for user in data[str(args[0])]["2"]["players"]:
-                    with open("eco.json", "r+") as fs:
-                        data2=json.load(fs); data2[str(user)] -= sum
+                    
+                    for user in data[str(args[0])]["2"]["players"]:
+                        data2[str(user)] -= sum
                         fs.seek(0); json.dump(data2, fs, indent=4); fs.truncate(); fs.close()
-                
-                for line in list(data):
-                    if str(args[0]) in line:
-                        del data[str(args[0])]
-                        f.seek(0); json.dump(data, f, indent=4); f.truncate(); f.close()
+                    
 
-
-            elif str(args[1]) == "2":
-                for user in data[str(args[0])]["2"]["players"]:
-                    user_count += 1
-                    sum += data[str(args[0])]["1"]["players"][str(user)] / user_count
-                    with open("eco.json", "r+") as fs:
-                        data2=json.load(fs); data2[str(user)] += sum
+                elif str(args[1]) == "2":
+                    for user in data[str(args[0])]["2"]["players"]:
+                        user_count += 1
+                        sum += data[str(args[0])]["1"]["players"][str(user)] / user_count
+                        data2[str(user)] += sum
                         fs.seek(0); json.dump(data2, fs, indent=4); fs.truncate(); fs.close()
-                
-                for user in data[str(args[0])]["1"]["players"]:
-                    with open("eco.json", "r+") as fs:
-                        data2=json.load(fs); data2[str(user)] -= sum
+                    
+                    for user in data[str(args[0])]["1"]["players"]:
+                        data2[str(user)] -= sum
                         fs.seek(0); json.dump(data2, fs, indent=4); fs.truncate(); fs.close()
 
 
@@ -114,7 +108,7 @@ async def end(ctx, *args):
                         del data[str(args[0])]
                         f.seek(0); json.dump(data, f, indent=4); f.truncate(); f.close()
 
-            await ctx.send(embed=discord.Embed(title='Bet Ended', description=f'**Owner: **{ctx.author.mention}\n **Correct Answer: **{str(args[1])}', color=65280))
+                await ctx.send(embed=discord.Embed(title='Bet Ended', description=f'**Owner: **{ctx.author.mention}\n **Correct Answer: **{str(args[1])}', color=65280))
         else:
             await ctx.send(embed=discord.Embed(description=f'{ctx.author.mention} you are not the owner of this bet', color=65280))
 
@@ -124,7 +118,7 @@ async def end(ctx, *args):
 
 @client.command()
 async def wallet(ctx):
-    with open("eco.json", "r+") as f:
+    with open("json/eco.json", "r+") as f:
         data=json.load(f)
         await ctx.send(embed=discord.Embed(title='My Wallet', description=f'**User: **{ctx.author.mention}\n**Money: **{data[str(ctx.author.id)]}$', color=65280))
 
@@ -133,7 +127,7 @@ async def wallet(ctx):
 
 @client.command()
 async def pay(ctx, *args):
-    with open("eco.json", "r+") as f:
+    with open("json/eco.json", "r+") as f:
         data=json.load(f)
         user_id = str(list(args)[0]).strip("<").strip(">").strip("@").replace('!', '')
         if data[str(ctx.author.id)] > 0:
@@ -142,6 +136,8 @@ async def pay(ctx, *args):
             await ctx.send(embed=discord.Embed(description=f'{ctx.author.mention} has sent {list(args)[1]} to {list(args)[0]}', color=65280))
         else:
             await ctx.send(embed=discord.Embed(description=f'{ctx.author.mention} does not have enough money', color=65280))
+
+
 
 
 
