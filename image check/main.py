38b5e44309705f27e811f1f30cbd306bc712da4f
Tristan <75189508+realTristan@users.noreply.github.com>
2021-08-09 22:29:14 -0400

    Update main.py

--- image check/main.py
+++ image check/main.py
@@ -1,7 +1,5 @@
-import discord, os, json, asyncio
+import discord
 from discord.ext import commands
-from discord.ext.commands import CommandNotFound, CommandInvokeError, has_permissions
-from discord.utils import get, find
 
 
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
@@ -14,28 +12,17 @@ async def on_ready():
     print(f'Launched: {client.user.name} // {client.user.id}')
 
 
-
+    
 @client.command()
-async def check(ctx):
-    messages = await ctx.message.channel.history(limit=200).flatten() # amount of messages you want to look at (200)
+async def check(ctx, user : discord.Member):
+    count = 0
+    messages = await ctx.message.channel.history().flatten()
     for msg in messages:
-        if msg.attachments:
-            with open("check.json", "r+") as f:
-                data=json.load(f)
-                if str(msg.author) not in data:
-                    data.update({str(msg.author) : 0})
-                    f.seek(0); json.dump(data, f, indent=5); f.truncate()
-                    data[str(msg.author)] += 1
-                else:
-                    data[str(msg.author)] += 1
-
-            for user in data:
-                await ctx.send(embed=discord.Embed(description=f'{user} has uploaded **{data[str(user)]}** images', color=65535))
-            f.close()
+        if msg.author == user and msg.attachments:
+            count += 1
+    await ctx.send(embed=discord.Embed(description=f'{user} has uploaded **{count}** images', color=65535))
 
 
 
-
-
-
+    
 client.run(YOUR_TOKEN)
