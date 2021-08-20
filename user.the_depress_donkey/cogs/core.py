32420e6c1d959a600d6ce137960edf305d86f717
Tristan <75189508+realTristan@users.noreply.github.com>
2021-08-20 10:59:34 -0400

    Create core.py

---
+++ user.the_depress_donkey/cogs/core.py
@@ -0,0 +1,43 @@
+import discord, json
+from discord.ext import commands
+
+class Core(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    # FUNCTION USED TO WRITE DATA TO THE JSON FILE
+    # ////////////////////////////////////////////
+    def write(self, file, data):
+        with open(f"{file}.json", "w") as f:
+            json.dump(data, f, indent=4)
+
+
+
+    # ON MEMBER JOIN, ADD THEM TO THE JSON FILE 
+    # /////////////////////////////////////////
+    @commands.Cogs.listener
+    async def on_member_join(self, member):
+        with open("people.json", "r+") as f:
+            data=json.load(f)
+            data["users"].append(member)
+            self.write("data", data)
+    
+
+
+    # ON MEMBER LEAVE, REMOVE THEM FROM THE JSON FILE
+    # ///////////////////////////////////////////////
+    @commands.Cogs.listener
+    async def on_member_leave(self, member):
+        with open("people.json", "r+") as f:
+            data=json.load(f)
+            data["users"].remove(member)
+            self.write("data", data)
+        
+        await member.send("Message you want to send them when they leave the server")
+
+
+
+# SETTING UP THE COG
+# //////////////////
+def setup(client):
+    client.add_cog(Core(client))
